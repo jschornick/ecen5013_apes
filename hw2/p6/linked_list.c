89bb74fa536cf260ee6929503cc1db32b1b2824a
Jeff Schornick <jeff@schornick.org>
2018-02-04 04:15:33 -0700

    [HW2] Linked list function defintions

--- hw2/p6/linked_list.c
+++ hw2/p6/linked_list.c
@@ -11,6 +11,9 @@
 
 #include <stdlib.h>  // malloc
 #include <stddef.h>
+
+#include <stdio.h>
+
 #include "linked_list.h"
 
 node_t * insert_at_beginning( node_t *p_head, node_t *p_new )
@@ -18,9 +21,163 @@ node_t * insert_at_beginning( node_t *p_head, node_t *p_new )
     // No matter what, our new node is the head, so mark it as such
     p_new->prev = NULL;
 
-    // Since we've been passed in the head, use it as the next node
+    // Since we've been passed in the head, mark it as the next node
     // in the chain
     p_new->next = p_head;
 
+    // back link the previous head to the new node if non-null
+    if( p_new->next )
+    {
+        p_new->next->prev = p_new;
+    }
+
     return p_new;
+}
+
+
+node_t * insert_at_end( node_t *p_head, node_t *p_new )
+{
+    // No matter what, our new node is the tail, so mark it as such
+    p_new->next = NULL;
+
+    node_t *p_last = get_tail(p_head);
+
+    // We know p_last was the previous tail, wire it up to the new tail
+    p_new->prev = p_last;
+
+    // and the corresponding link back
+    if( p_new->prev )
+    {
+        p_new->prev->next = p_new;
+    }
+
+    return p_head;
+}
+
+node_t * insert_at_position( node_t *p_base, node_t *p_new, int32_t offset )
+{
+
+    node_t *p_node;
+
+    p_node = get_offset( p_base, offset );
+
+    if( p_node ) {
+        p_new->next = p_node;
+        p_new->prev = p_node->prev;
+
+        if (p_new->prev) {
+            p_new->prev->next = p_new;
+        }
+        p_node->prev = p_new;
+    }
+
+
+    return get_head(p_new);
+}
+
+
+node_t * delete_from_beginning( node_t *p_head )
+{
+    if( p_head )
+    {
+        // if there is a node following head, it's now the new head
+        if( p_head->next )
+        {
+            p_head->next->prev = NULL;
+        }
+        p_head = p_head->next;
+    }
+    return p_head;
+}
+
+node_t * delete_from_end( node_t *p_head )
+{
+
+    node_t * p_tail = get_tail(p_head);
+    if( p_tail )
+    {
+        // if there is a node before tail, it's now the new tail
+        if( p_tail->prev )
+        {
+            p_tail->prev->next = NULL;
+        }
+        p_head = get_head(p_tail);
+    }
+    return p_head;
+}
+
+
+node_t * delete_from_position( node_t *p_base, int32_t offset )
+{
+    node_t *p_node;
+
+    p_node = get_offset( p_base, offset );
+
+    if( p_node )
+    {
+        if( p_node->prev ) {
+            p_node->prev->next = p_node->next;
+        }
+        if( p_node->next ) {
+            p_node->next->prev = p_node->prev;
+        }
+    }
+
+    return get_head(p_node);
+
+}
+
+size_t size( node_t *p_node )
+{
+    size_t count = 0;
+    p_node = get_head(p_node);
+    while( p_node ) {
+        p_node = p_node->next;
+        count++;
+    }
+    return count;
+}
+
+node_t * get_head( node_t *p_node )
+{
+    node_t *p_last = p_node;
+    // walk list backward until at head
+    while( p_node )
+    {
+        p_last = p_node;
+        p_node = p_node->prev;
+    }
+
+    return p_last;
+}
+
+node_t * get_tail( node_t *p_node )
+{
+    node_t *p_prev = p_node;
+    // walk list until at end
+    while( p_node )
+    {
+        p_prev = p_node;
+        p_node = p_node->next;
+    }
+
+    return p_prev;
+}
+
+node_t * get_offset( node_t *p_node, int32_t offset )
+{
+    if( !offset ) {
+        return p_node;
+    }
+    while( p_node && (offset > 0) )
+    {
+        p_node = p_node->next;
+        offset--;
+    }
+    while( p_node && (offset < 0) )
+    {
+        p_node = p_node->prev;
+        offset++;
+    }
+    return p_node;
 }
--- hw2/p6/linked_list.h
+++ hw2/p6/linked_list.h
@@ -13,6 +13,7 @@
 #define _LINKED_LIST_H
 
 #include <stddef.h>  // offsetof
+#include <stdint.h>
 
 typedef struct node node_t;
 typedef struct node {
@@ -31,6 +32,17 @@ typedef struct node {
  * dereferenced to access the structure's data elements.
  */
 #define LIST_CONTAINER(addr, type, member) ( (type *) ( ((void *) addr) - offsetof(type,member) ) )
+
+#define DESTROY( p_head, type, member ) (                   \
+        {                                                   \
+            node_t *p_next;                                 \
+            while(p_head) {                                 \
+                p_next = p_head->next;                      \
+                free(LIST_CONTAINER(p_head, type, member)); \
+                p_head = p_next;                            \
+            }                                               \
+        }                                                   \
+        )
 
 #define NODE_INIT(p_node) ( p_node->prev = NULL; p_node->next = NULL )
 
@@ -64,11 +76,11 @@ node_t * insert_at_end( node_t *p_head, node_t *p_new );
  * Function    : insert_at_position
  * Description :​ Add a node to the given position of the list
  * Params      :​ @p_base pointer to the base node
- *               @p_new data to add
- *               @index offset of where to add the data
+ *               @p_new  data to add
+ *               @offset offset describing where to add the data
  * Returns     :​ Pointer to the head of the linked list
  */
-node_t * insert_at_position( node_t *p_base, node_t *p_new, size_t index );
+node_t * insert_at_position( node_t *p_base, node_t *p_new, int32_t offset );
 
 /**
  * Function    : delete_from_beginning
@@ -90,18 +102,46 @@ node_t * delete_from_end( node_t *p_head );
  * Function    : delete_from_position
  * Description :​ Delete the node at a given position in the list
  * Params      :​ @p_base pointer to the head node
- *               @index offset of where to remove the data
+ *               @offset offset describing where to remove the data
  * Returns     :​ Pointer to the head of the linked list (or NULL)
  */
-node_t * delete_from_position( node_t *p_base, size_t index );
+node_t * delete_from_position( node_t *p_base, int32_t index );
 
 /**
  * Function    : size
  * Description :​ Return the number of nodes in the list
  * Params      :​ @p_node A node pointer in the list
  * Returns     :​ Number of nodes
+ *
+ * NOTE: This function is dangerous and makes little sense as specified.
+ * The onus is left on the caller to clean up the allocated container
+ * memory, but is not given any indication which item was deleted from
+ * the list.
  */
 size_t size( node_t *p_node );
 
+/**
+ * Function    : get_head
+ * Description :​ returns the node at the head of the list
+ * Params      :​ @p_node pointer to a node in the list
+ * Returns     :​ Pointer to the head of the linked list (or NULL)
+ */
+node_t * get_head( node_t *p_node );
+
+/**
+ * Function    : get_tail
+ * Description :​ returns the node at the tail of the list
+ * Params      :​ @p_node pointer to a node in the list
+ * Returns     :​ Pointer to the head of the linked list (or NULL)
+ */
+node_t * get_tail( node_t *p_node );
+
+/**
+ * Function    : get_offset
+ * Description :​ returns the node at the tail of the list
+ * Params      :​ @p_node pointer to a node in the list
+ * Returns     :​ Pointer to the head of the linked list (or NULL)
+ */
+node_t * get_offset( node_t *p_node, int32_t offset );
 
 #endif /* _LINKED_LIST_H */
