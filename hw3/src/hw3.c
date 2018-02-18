8d5bd7e09aa80d9c9e1bbe0b792f16092f894261
Jeff Schornick <jeff@schornick.org>
2018-02-18 00:24:57 -0700

    [HW3] Use TLS to store thread_info pointer

--- hw3/src/hw3.c
+++ hw3/src/hw3.c
@@ -43,10 +43,15 @@ const char *thread_name[] = { "Main", "T1", "T2" };
 
 thread_info_t thread_info[THREAD_MAX];
 
-void logit( thread_info_t *tinfo, const char *format, ... );
+void logit( const char *format, ... );
+void log_with_tinfo( thread_info_t *tinfo, const char*format, ...);
+void log_varg( thread_info_t *tinfo, const char*format, va_list args);
+
 void thread_init( thread_info_t *tinfo);
 void *tf1( void *data );
 void *tf2( void *data );
+
+pthread_key_t tinfo_key;
 
 // finishes tinfo population and opens log
 void thread_init( thread_info_t *tinfo)
@@ -55,19 +60,35 @@ void thread_init( thread_info_t *tinfo)
     tinfo->pthread_tid = pthread_self();
     tinfo->name = thread_name[tinfo->app_tid];
 
+    struct timespec t;
+    clock_gettime(CLOCK_MONOTONIC, &t );
+
+    pthread_setspecific(tinfo_key, tinfo );
+
     tinfo->p_logfile = fopen( tinfo->log_filename, "a" );
-    logit( tinfo, "Opened logfile, fd = %d\n", fileno(tinfo->p_logfile));
-    logit( tinfo, "App TID: %u, Linux TID: %d, pthread ID: %lu\n",
+    logit( "Opened logfile, fd = %d\n", fileno(tinfo->p_logfile));
+    logit( "App TID: %u, Linux TID: %d, pthread ID: %lu\n",
            tinfo->app_tid, tinfo->linux_tid, tinfo->pthread_tid );
+
+    logit( "Start time: %u.%09u\n", t.tv_sec, t.tv_nsec );
+}
+
+void thread_cleanup(void *data)
+{
+    thread_info_t *tinfo = data;
+    struct timespec t;
+    clock_gettime(CLOCK_MONOTONIC, &t );
+    // our thread local storage is no longer valid, but the allocation is still valid
+    log_with_tinfo(tinfo, "Exit time: %u.%09u\n", t.tv_sec, t.tv_nsec );
 }
 
 int main( int argc, char *argv[] )
 {
-    //pthread_attr_t attr;
-    //pthread_attr_init(&attr);
 
     thread_info_t *tinfo;
     tinfo = &thread_info[THREAD_MAIN];
+
+    pthread_key_create(&tinfo_key, thread_cleanup);
 
     // Get log file name from the command line or use a default
     // We're populating the name in tinfo since the assignment requires that
@@ -75,7 +96,7 @@ int main( int argc, char *argv[] )
     if ( argc < 2 ) {
         tinfo->log_filename = default_logfile;
     } else {
-        thread_info[THREAD_MAIN].log_filename = argv[1];
+        tinfo->log_filename = argv[1];
     }
 
     for(thread_name_t name = 0; name<THREAD_MAX; name++) {
@@ -87,7 +108,7 @@ int main( int argc, char *argv[] )
     pthread_mutex_init(&log_mutex, NULL);  // NULL == default attrs
 
     // Even though this is the main thread, initiailze our info just like any other thread
-    thread_init(&thread_info[THREAD_MAIN]);
+    thread_init(tinfo);
 
     // *** main init done ***
 
@@ -95,16 +116,19 @@ int main( int argc, char *argv[] )
 
     // thread struct, thread attrs, func, arg
     if( pthread_create( &t1, NULL, tf1, &thread_info[THREAD_1] ) ) {
-        logit( tinfo, "Thread 1 create fail!\n" );
+        logit( "Thread 1 create fail!\n" );
     }
 
     if( pthread_create( &t2, NULL, tf2, &thread_info[THREAD_2] ) ) {
-        logit( tinfo, "Thread 2 create fail!\n" );
+        logit( "Thread 2 create fail!\n" );
     }
 
     // thread struct, void **retval
     pthread_join( t1, NULL );
     pthread_join( t2, NULL );
+
+    // register with atexit?
+    pthread_exit(0);
 
     return 0;
 }
@@ -117,7 +141,7 @@ void *tf1( void *data )
     thread_init( tinfo );
 
     for( int i=0; i<2000; i++ ) {
-        logit( tinfo, "Thread 1 is the first thread\n" );
+        logit( "Thread 1 is the first thread\n" );
     }
     return NULL;
 }
@@ -129,25 +153,42 @@ void *tf2( void *data )
     thread_init( tinfo );
 
     for( int i=0; i<2000; i++ ) {
-        logit( tinfo, "Thread 2 is a happy little thread\n" );
+        logit( "Thread 2 is a happy little thread\n" );
     }
     return NULL;
 }
 
 
-// We can't be 100% certain when the file stream will be flushed, so append to the stream and flush in one atomic action
-// this guantees that complete log lines are added to the log file
-void logit( thread_info_t *tinfo, const char *format, ... )
+void logit( const char *format, ... )
 {
     va_list args;
     va_start(args, format); // populate va_list with optional args
 
-    pthread_mutex_lock(&log_mutex);
-    fprintf( tinfo->p_logfile, "[%s] ", tinfo->name);
-    vfprintf( tinfo->p_logfile, format, args );
-    fflush( tinfo->p_logfile );
-    pthread_mutex_unlock(&log_mutex);
+    thread_info_t *tinfo = pthread_getspecific(tinfo_key);
+    log_varg(tinfo, format, args );
 
     va_end(args);  // cleanup
 }
 
+
+void log_with_tinfo( thread_info_t *tinfo, const char*format, ...)
+{
+
+    va_list args;
+    va_start(args, format); // populate va_list with optional args
+
+    log_varg(tinfo, format, args );
+
+    va_end(args);  // cleanup
+}
+
+void log_varg( thread_info_t *tinfo, const char*format, va_list args)
+{
+    // We can't be 100% certain when the file stream will be flushed, so append to the stream and flush in one atomic action
+    // this guantees that complete log lines are added to the log file
+        pthread_mutex_lock(&log_mutex);
+    fprintf( tinfo->p_logfile, "[%s] ", tinfo->name);
+    vfprintf( tinfo->p_logfile, format, args );
+    fflush( tinfo->p_logfile );
+    pthread_mutex_unlock(&log_mutex);
+}
